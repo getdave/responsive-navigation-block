d79a3ab1c7fcec63edb35018742ae7fc70a4a003
Dave Smith <getdavemail@gmail.com>
2024-04-12 12:29:47 +0100

    Update classNames

--- plugin.php
+++ plugin.php
@@ -13,7 +13,6 @@
  * @package getdave
  */
 
-
 namespace GetDave\ResponsiveNavBlockVariations;
 
 // If this file is called directly, abort.
@@ -21,63 +20,65 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+/**
+ * Initialize the plugin.
+ */
 function init() {
-    add_action( 'init', __NAMESPACE__ . '\register_assets' );
-    add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
+	add_action( 'init', __NAMESPACE__ . '\register_assets' );
+	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
 }
 
 /**
  * Register the assets.
  */
 function register_assets() {
-    $asset_file = plugin_dir_path( __FILE__ ) . '/build/index.asset.php';
+	$asset_file = plugin_dir_path( __FILE__ ) . '/build/index.asset.php';
 
-    if ( file_exists( $asset_file ) ) {
-        $assets = include $asset_file;
+	if ( file_exists( $asset_file ) ) {
+		$assets = include $asset_file;
 
-        // register the script
-        wp_register_script(
-            'getdave-responsive-nav-block-variations-script',
-            plugins_url( 'build/index.js', __FILE__ ),
-            $assets['dependencies'],
-            $assets['version']
-        );
+		wp_register_script(
+			'getdave-responsive-nav-block-variations-script',
+			plugins_url( 'build/index.js', __FILE__ ),
+			$assets['dependencies'],
+			$assets['version']
+		);
 
-        wp_enqueue_block_style(
-            'core/navigation',
-            array(
-                'handle' => 'getdave-responsive-nav-block-variations-style',
-                'src' => plugins_url( 'build/style-index.css', __FILE__ ),
-                // Allow Themes to opt into inlining the style.
-                // See https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/#parameters.
-                'path' => plugin_dir_path( __FILE__ ) . 'build/style-index.css',
-                $assets['version']
-            )
-        );
-    }
+		wp_enqueue_block_style(
+			'core/navigation',
+			array(
+				'handle' => 'getdave-responsive-nav-block-variations-style',
+				'src'    => plugins_url( 'build/style-index.css', __FILE__ ),
+				// Allow Themes to opt into inlining the style.
+				// See https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/#parameters.
+				'path'   => plugin_dir_path( __FILE__ ) . 'build/style-index.css',
+				$assets['version'],
+			)
+		);
+	}
 }
 
 /**
  * Enqueue the editor assets.
  */
 function enqueue_block_editor_assets() {
-    wp_enqueue_script(
-        'getdave-responsive-nav-block-variations-script',
-    );
+	wp_enqueue_script(
+		'getdave-responsive-nav-block-variations-script',
+	);
 
-    // Inline variables for access in JavaScript.
-    $inline_variables = array(
-        'classNames' => array(
-            'mobile' => 'is-style-getdave-navigation-mobile',
-            'desktop' => 'is-style-getdave-navigation-desktop',
-        ),
-    );
+	// Inline variables for access in JavaScript.
+	$inline_variables = array(
+		'classNames' => array(
+			'mobile'  => 'getdave-responsive-nav-block-variations-mobile',
+			'desktop' => 'getdave-responsive-nav-block-variations-desktop',
+		),
+	);
 
-    wp_localize_script(
-        'getdave-responsive-nav-block-variations-script',
-        'getdaveResponsiveNavBlockVariations',
-        $inline_variables
-    );
+	wp_localize_script(
+		'getdave-responsive-nav-block-variations-script',
+		'getdaveResponsiveNavBlockVariations',
+		$inline_variables
+	);
 }
 
 init();
