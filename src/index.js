5bcfe329ffad71a91bdad7ce041b0cef8607fb8a
Dave Smith <getdavemail@gmail.com>
2024-04-15 09:48:58 +0100

    Code quality

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 // WordPress dependencies.
-import { registerBlockVariation } from "@wordpress/blocks";
+import { registerBlockVariation } from '@wordpress/blocks';
 
 // Local dependencies.
-import "./mobile-view-switcher";
-import "./style.scss";
+import './mobile-view-switcher';
+import './style.scss';
 
 // Data inlined from PHP.
 const { mobile: mobileClassName, desktop: desktopClassName } =
 	getdaveResponsiveNavBlockVariations.classNames;
 
-registerBlockVariation("core/navigation", {
-	name: "getdave-navigation-desktop",
-	title: "Desktop Navigation",
-	description: "Navigation block preconfigured for larger viewports.",
+registerBlockVariation( 'core/navigation', {
+	name: 'getdave-navigation-desktop',
+	title: 'Desktop Navigation',
+	description: 'Navigation block preconfigured for larger viewports.',
 	attributes: {
-		overlayMenu: "never",
+		overlayMenu: 'never',
 		className: desktopClassName,
 	},
-	isActive(blockAttributes) {
+	isActive( blockAttributes ) {
 		return (
-			blockAttributes.className?.includes(desktopClassName) &&
-			blockAttributes.overlayMenu === "never"
+			blockAttributes.className?.includes( desktopClassName ) &&
+			blockAttributes.overlayMenu === 'never'
 		);
 	},
-});
+} );
 
-registerBlockVariation("core/navigation", {
-	name: "getdave-navigation-mobile",
-	title: "Mobile Navigation",
-	description: "Navigation block preconfigured for smaller viewports.",
+registerBlockVariation( 'core/navigation', {
+	name: 'getdave-navigation-mobile',
+	title: 'Mobile Navigation',
+	description: 'Navigation block preconfigured for smaller viewports.',
 	attributes: {
-		overlayMenu: "always",
+		overlayMenu: 'always',
 		className: mobileClassName,
 	},
-	isActive(blockAttributes) {
+	isActive( blockAttributes ) {
 		return (
-			blockAttributes.className?.includes(mobileClassName) &&
-			blockAttributes.overlayMenu === "always"
+			blockAttributes.className?.includes( mobileClassName ) &&
+			blockAttributes.overlayMenu === 'always'
 		);
 	},
-});
+} );
--- src/mobile-view-switcher.js
+++ src/mobile-view-switcher.js
@@ -1,49 +1,55 @@
-import { registerPlugin } from "@wordpress/plugins";
-import { useSelect, useDispatch } from "@wordpress/data";
-import { useEffect } from "@wordpress/element";
-import { store as editorStore } from "@wordpress/editor";
-import { store as blockEditorStore } from "@wordpress/block-editor";
+import { registerPlugin } from '@wordpress/plugins';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import { store as editorStore } from '@wordpress/editor';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+
+// Data inlined from PHP.
+const { mobile: mobileClassName } =
+	getdaveResponsiveNavBlockVariations.classNames;
 
 const MobileViewSwitcher = () => {
-	const selectedNavigationBlock = useSelect((select) => {
+	const selectedNavigationBlock = useSelect( ( select ) => {
 		const { getSelectedBlock, getBlock, getBlockParentsByBlockName } =
-			select(blockEditorStore);
+			select( blockEditorStore );
 		const block = getSelectedBlock();
 
-		if (!block) {
+		if ( ! block ) {
 			return false;
 		}
 
 		if (
-			block?.name === "core/navigation" &&
-			block.attributes?.className?.includes(mobileClassName)
+			block?.name === 'core/navigation' &&
+			block.attributes?.className?.includes( mobileClassName )
 		) {
 			return true;
 		}
 
 		const parentIds = getBlockParentsByBlockName(
 			block.clientId,
-			"core/navigation",
+			'core/navigation'
 		);
-		return parentIds.some((id) => {
-			const parentBlock = getBlock(id);
-			return parentBlock?.attributes?.className?.includes(mobileClassName);
-		});
-	}, []);
+		return parentIds.some( ( id ) => {
+			const parentBlock = getBlock( id );
+			return parentBlock?.attributes?.className?.includes(
+				mobileClassName
+			);
+		} );
+	}, [] );
 
-	const { setDeviceType } = useDispatch(editorStore);
+	const { setDeviceType } = useDispatch( editorStore );
 
-	useEffect(() => {
-		if (selectedNavigationBlock) {
+	useEffect( () => {
+		if ( selectedNavigationBlock ) {
 			// Switch the editor to mobile view
-			setDeviceType("Mobile");
+			setDeviceType( 'Mobile' );
 		} else {
 			// Switch the editor to desktop view
-			setDeviceType("Desktop");
+			setDeviceType( 'Desktop' );
 		}
-	}, [selectedNavigationBlock]);
+	}, [ selectedNavigationBlock, setDeviceType ] );
 
 	return null;
 };
 
-registerPlugin("mobile-view-switcher", { render: MobileViewSwitcher });
+registerPlugin( 'mobile-view-switcher', { render: MobileViewSwitcher } );
